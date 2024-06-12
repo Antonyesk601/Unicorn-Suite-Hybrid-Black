02d5f6d05de27a9f8a3661e5593295f977e0b51b
Myriam2002 <ma.barakat@nu.edu.eg>
2024-06-12 08:59:47 +0200

    Yasmeen Amr Data

--- RecorderApp/PromptViewer.py
+++ RecorderApp/PromptViewer.py
@@ -265,7 +265,7 @@ if __name__ == "__main__":
     print(recordSets)
 
     exp = ExperimentConfig(
-        ExperimentOrder=recordSets, SubjectID="AmrMustafa/AmrMustafa"
+        ExperimentOrder=recordSets, SubjectID="YasmeenAmr/YasmeenAmr"
     )
     
     viewer = PromptViewer(
